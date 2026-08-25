b23f9a0150c31e55dc81146742dd49d3ab6a454e
praveenrar451-hash <praveenrar451@gmail.com>
2026-08-25 16:03:51 +0530

    Update user registration logic with fixed password
    Refactor user registration to enforce a fixed password and update error messages.

--- index.js
+++ index.js
@@ -8,47 +8,28 @@ const io = new Server(server);
 
 app.use(express.static('public'));
 
-// Store registered users and active sessions
-const users = {}; // username -> password
-const activeUsers = {}; // socket.id -> username
+const activeUsers = {};
+const FIXED_PASSWORD = "272009";
 
 io.on('connection', (socket) => {
-    console.log('User connected:', socket.id);
-
-    // Register user
     socket.on('register', ({ username, password }, callback) => {
-        if (!username || !password) return callback({ success: false, message: 'Fill all fields' });
-        
-        if (users[username]) {
-            if (users[username] === password) {
-                activeUsers[socket.id] = username;
-                callback({ success: true, message: 'Login successful' });
-                io.emit('update_users', Object.values(activeUsers));
-            } else {
-                callback({ success: false, message: 'Incorrect password!' });
-            }
-        } else {
-            users[username] = password;
-            activeUsers[socket.id] = username;
-            callback({ success: true, message: 'Account created & logged in!' });
-            io.emit('update_users', Object.values(activeUsers));
+        if (!username) return callback({ success: false, message: 'Username daalna zaroori hai!' });
+        if (password !== FIXED_PASSWORD) {
+            return callback({ success: false, message: 'Galat Password! Sahi password "272009" hai.' });
         }
+        
+        activeUsers[socket.id] = username;
+        callback({ success: true });
+        io.emit('update_users', Object.values(activeUsers));
     });
 
-    // Chat Message
     socket.on('chat_message', (data) => {
         io.emit('chat_message', data);
-    });
-
-    // WebRTC Signaling for Real-time Calls
-    socket.on('call_user', (data) => {
-        socket.broadcast.emit('incoming_call', data);
     });
 
     socket.on('disconnect', () => {
         delete activeUsers[socket.id];
         io.emit('update_users', Object.values(activeUsers));
-        console.log('User disconnected');
     });
 });
 
@@ -56,4 +37,3 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-
